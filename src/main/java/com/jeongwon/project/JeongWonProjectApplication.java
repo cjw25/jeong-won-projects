f2fb14e86928692dc690ef6049731a7a52a24d04
cjw25 <cnjzzan@naver.com>
2026-09-10 17:48:29 +0900

    Jeong Won

---
+++ src/main/java/com/jeongwon/project/JeongWonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jeongwon.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeongWonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeongWonProjectApplication.class, args);
+	}
+
+}
